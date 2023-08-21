98bfc037d637a14bbea876a86bdc33f0584fe922
unknown <nilton.oliveira0305@gmail.com>
2023-08-21 15:57:56 -0300

    build

--- src/util/tomorrowAt3pm.ts
+++ src/util/tomorrowAt3pm.ts
@@ -1,7 +1,7 @@
 export function tomorrowAt3pm() {
     const brazilTimezoneOffset = -3;
     const tomorrow = new Date();
-    tomorrow.setUTCDate(tomorrow.getDate());
+    tomorrow.setUTCDate(tomorrow.getDate() + 1);
     tomorrow.setUTCHours(15 - brazilTimezoneOffset, 0, 0, 0);
     return tomorrow;
 }
