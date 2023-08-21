b075085c22ccc3abbda5487a13d1a0d0c99c6a45
unknown <nilton.oliveira0305@gmail.com>
2023-08-21 09:21:52 -0300

    build

--- src/app/api/kick-movie/route.ts
+++ src/app/api/kick-movie/route.ts
@@ -5,15 +5,14 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
     const body = await req.json()
 
-    const { username } = file.readJson('db')
+    const db = file.readJson('db')
 
-    if (body.movie.toUpperCase() !== username.toUpperCase()) {
+    if (body.movie.toUpperCase() !== db?.username?.toUpperCase()) {
         cookiesBack.delete('KICK_MOVIE')
         return NextResponse.json({ status: 418 })
     }
 
-    cookiesBack.set('KICK_MOVIE', username)
-    const db = file.readJson('db')
+    cookiesBack.set('KICK_MOVIE', db?.username?.username)
     file.createJson({ ...db, count: db.count + 1 }, 'db')
     return NextResponse.json({ message: 'its the movie', status: 200 })
 }
