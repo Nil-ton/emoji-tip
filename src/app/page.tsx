f8924602e9e786bca2d6641a1e2948c7550538a0
unknown <nilton.oliveira0305@gmail.com>
2023-08-21 08:43:42 -0300

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { Count } from '@/components/Count'
 import { EmojiTip } from '../components/EmojiTip'
 import { FieldMovieSearch } from '../components/FieldMovieSearch'
 import { FormLogin } from '../components/FormLogin'
-export const dynamic = 'force-dynamic'
 
 export default async function Home() {
   return (
