78b3f71cd076f4e5f3ed7f1d54db9902b9be5273
unknown <nilton.oliveira0305@gmail.com>
2023-08-21 10:26:42 -0300

    path.resolve

--- src/services/writeFile/writeFile.ts
+++ src/services/writeFile/writeFile.ts
@@ -4,10 +4,10 @@ import path from 'path';
 export const file = {
     createJson(data: any, filename: string) {
         const transformInString = JSON.stringify(data, null, 2);
-        fs.writeFileSync(path.join(__dirname, `./public/${filename}.json`), transformInString, { encoding: 'utf-8' });
+        fs.writeFileSync(path.resolve(`./public/${filename}.json`), transformInString, { encoding: 'utf-8' });
     },
     readJson(filename: string) {
-        const arquivo = fs.readFileSync(path.join(__dirname, `./public/${filename}.json`), 'utf-8');
+        const arquivo = fs.readFileSync(path.resolve(`./public/${filename}.json`), 'utf-8');
         return arquivo ? JSON.parse(arquivo || '') : ''
     },
 }
